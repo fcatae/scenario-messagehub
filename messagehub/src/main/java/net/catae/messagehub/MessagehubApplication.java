51de348d5205de82b8acef6ad35a67907ea89c85
Fabricio Catae <fcatae@microsoft.com>
2018-11-03 21:44:09 -0300

    initial commit

---
+++ messagehub/src/main/java/net/catae/messagehub/MessagehubApplication.java
@@ -0,0 +1,12 @@
+package net.catae.messagehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagehubApplication.class, args);
+	}
+}
