edec024e3c15b47babd703699d58770b06cab67e
Fabricio Catae <fcatae@microsoft.com>
2018-11-04 14:47:52 -0200

    implement branch

--- messagehub/src/main/java/net/catae/messagehub/Branch.java
+++ messagehub/src/main/java/net/catae/messagehub/Branch.java
@@ -1,16 +1,49 @@
 package net.catae.messagehub;
 
+import java.security.InvalidParameterException;
+
 public class Branch {
-	public void credit(Account a, int amount) {		
+	
+	private String name;
+	private int amount;
+	
+	public Branch(String name, int amount) {		
+		this.name = name;
+		this.amount = amount;
 	}
 	
-	public void debit(Account a, int amount) {		
+	public void credit(Account a, int value) {		
+		if(a == null) 
+			throw new InvalidParameterException("account == null");
+
+		a.credit(value);
+		this.amount -= value;
 	}
 	
-	public void transfer(Account a, Account b, int amount) {		
+	public void debit(Account a, int value) {
+		if(a == null) 
+			throw new InvalidParameterException("account == null");
+
+		a.credit(-value);
+		this.amount += value;
+	}
+	
+	public void transfer(Account a, Account b, int value) {
+		if(a == null || b == null) 
+			throw new InvalidParameterException("account == null");
+
+		if(a == b) 
+			throw new InvalidParameterException("account1 == account2");
+
+		a.credit(-value);
+		b.credit(+value);
+	}
+	
+	public String getName() {
+		return this.name;
 	}
 	
 	public int total() {	
-		return 0;
+		return this.amount;
 	}
 }
--- messagehub/src/test/java/net/catae/messagehub/BankTests.java
+++ messagehub/src/test/java/net/catae/messagehub/BankTests.java
@@ -2,8 +2,10 @@ package net.catae.messagehub;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 
-import org.junit.Assert;
+import java.security.InvalidParameterException;
+
 import org.junit.Test;
 
 public class BankTests {
@@ -29,5 +31,100 @@ public class BankTests {
 		// Check negative credit
 		a2.credit(-40);
 		assertEquals(a2.getAmount(), 60);
+	}
+
+	@Test
+	public void TestBranch() {
+		Account a1 = new Account("a1");
+		Account a2 = new Account("a2");
+		
+		// Branch: b = 1000
+		Branch b = new Branch("b", 1000);
+
+		// Transaction 1: b -> 600 -> a1
+		// b = 400
+		// a1 = 600
+		b.credit(a1, 600);
+		assertEquals("transfer a1 <- 600", a1.getAmount(), 600);
+		assertEquals("b has still 400", b.total(), 400);
+
+		// Transaction 2: a1 -> 150 -> a2
+		// a1 = 450
+		// a2 = 150
+		b.transfer(a1, a2, 150);
+		assertEquals("a1 has still 450", a1.getAmount(), 450);
+		assertEquals("transfer a2 <- 150", a2.getAmount(), 150);
+
+		// Transaction 3: a2 -> 50 -> b
+		// a2 = 100
+		// b = 450
+		b.debit(a2, 50);
+		assertEquals("a2 has still 100", a2.getAmount(), 100);
+		assertEquals("transfer b <- 50", b.total(), 450);
+	}
+
+	@Test
+	public void TestBranchNegativeValues() {
+		Account a = new Account("a");
+		Branch b = new Branch("b", 1000);
+
+		// perform negative operations
+		b.credit(a, -100);
+		b.debit(a, -50);
+		assertEquals(b.total(), 950);
+
+		// check negative balance
+		b.credit(a, 1000);
+		assertEquals(b.total(), -50);
+
+		// perform zero operation
+		b.credit(a, 0);
+		b.debit(a, 0);
+
+		// test initialization
+		Branch b0 = new Branch("b1", 0);
+		Branch bneg = new Branch("b2", -100);
+
+		assertEquals(b0.total(), 0);
+		assertEquals(bneg.total(), -100);
+	}
+	
+	@Test
+	public void TestBranchInvalidParameters() {
+		Account a1 = new Account("a1");
+		Branch b = new Branch("b", 1000);
+
+		assertThrown(IllegalArgumentException.class, ()->{
+			b.credit(null, 1);			
+		});
+		assertThrown(IllegalArgumentException.class, ()->{
+			b.debit(null, 2);			
+		});
+		assertThrown(IllegalArgumentException.class, ()->{
+			b.transfer(a1, null, 4);
+		});
+		assertThrown(IllegalArgumentException.class, ()->{
+			b.transfer(null, a1, 8);
+		});		
+		assertThrown(IllegalArgumentException.class, ()->{
+			b.transfer(a1, a1, 16);
+		});
+
+		assertEquals(a1.getAmount(), 0);
+		assertEquals(b.total(), 1000);
+	}
+
+	void assertThrown(Class<? extends Exception> cex, Action action) {
+		try {
+			action.exec();
+			throw new Exception();
+		}
+		catch(Exception ex) {
+			assertTrue(cex.isInstance(ex));
+		}
+	}
+
+	interface Action {
+		void exec();
 	}
 }
